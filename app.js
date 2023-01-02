82af259aa04b12a2d39ced8fd939433b36dc4d8d
raju <rajudampoori@gmail.com>
2023-01-02 19:53:08 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const express = require("express");
+// const router = require("./routers/routes")
+const cors = require("cors")
+mongoose.connect("mongodb+srv://root:rootroot@cluster0.ziwx6e8.mongodb.net/?retryWrites=true&w=majority")
+//mongodb://localhost:27017/realestate
+.then(()=> {console.log("connection success")})
+.catch((err)=> console.log(`Something went Wrong: ${err}`))
+
+const app = express();
+// app.use("/",router)
+app.use(cors())
+const port = process.env.PORT || 8000
+require('./models/basicinfo')
+require('./models/propertydetails')
+require('./models/generalinfo')
+require('./models/listing')
+
+app.use(express.json());
+app.use(require('./routers/routes'))
+app.listen(port,()=> {console.log(`Server is up at ${port}`)})
